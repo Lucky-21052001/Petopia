150c0845bd17633e5cfeffb1130c92fd911dd72c
Lucky-21052001 <120821905+Lucky-21052001@users.noreply.github.com>
2023-07-21 18:23:44 +0530

    Update footer.php

--- ecommerce/components/footer.php
+++ ecommerce/components/footer.php
@@ -22,7 +22,7 @@
          <h3>contact us</h3>
          <a href="tel:1234567890"><i class="fas fa-phone"></i> +123 456 7899</a>
          <a href="tel:11122233333"><i class="fas fa-phone"></i> +111 222 3333</a>
-         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> vaishanvi@gmail.com</a>
+         <a href="mailto:Lucky@gmail.com"><i class="fas fa-envelope"></i> Lucky@gmail.com</a>
          <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Pune, india - 400104 </a>
       </div>
 
